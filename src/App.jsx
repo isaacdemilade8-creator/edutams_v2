3dc0c70ff56e9646a3d278114dc71c6a780d6292
isaacdemilade8-creator <isaacdemilade8@gmail.com>
2026-08-12 18:30:45 +0100

    Genesis commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TeacherDashboard from './teacher-dashboard/TeacherDashboard.jsx'
+
+const App = () => {
+  return (
+    <>
+      <TeacherDashboard />
+    </>
+  )
+}
+
+export default App
